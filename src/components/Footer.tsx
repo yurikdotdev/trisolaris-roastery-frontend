64c2d1dc8d42136fee548ad95f4485450fad7db9
yurikdotdev <172495341+yurikdotdev@users.noreply.github.com>
2024-11-07 20:56:09 +0700

    feat: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <div className="absolute bottom-0 w-full bg-green-300 py-4 mt-auto">
+      <div className="text-center">
+        <p>&copy; 2024 Trisolaris Coffee. All rights reserved.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
+import Container from '@/components/Container';
+import Footer from '@/components/Footer';
 import TopBar from '@/components/TopBar';
 import Header from './components/Header';
-import Container from '@/components/Container';
 
 function Home() {
   return (
-    <Container className='bg-gray-500 h-screen'>
+    <Container className="h-screen bg-gray-500">
       <TopBar />
       <Header />
+      <Footer />
     </Container>
   );
 }
