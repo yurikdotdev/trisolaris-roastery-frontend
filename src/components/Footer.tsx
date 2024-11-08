33f61c4d352b1086aa3c0d98e613bd32d1a1261d
yurikdotdev <172495341+yurikdotdev@users.noreply.github.com>
2024-11-08 10:35:38 +0700

    feat: implement basic about page layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,9 @@
+import { Link } from 'react-router-dom';
+
 function Footer() {
   return (
     <div className="bottom-0 w-full bg-green-300 py-4 mt-auto">
+      <Link to={'/about'}>About Us</Link>
       <div className="text-center">
         <p>&copy; 2024 Trisolaris Coffee. All rights reserved.</p>
       </div>
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,6 +1,30 @@
+import Container from '@/components/Container';
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import TopBar from '@/components/TopBar';
+
 function About() {
   return (
-    <div>About</div>
-  )
+    <Container>
+      <TopBar />
+      <Header title="About" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quas
+          quibusdam. Atque, officia. Quod, quas quibusdam. Atque, officia. Quod,
+          quas quibusdam. Atque, officia. Quod, quas quibusdam. Atque, officia.
+          Quod, quas quibusdam. Atque, officia. Quod, quas quibusdam. Atque,
+          officia. Quod, quas quibusdam. Atque, officia. Quod, quas quibusdam.
+          Atque, officia. Quod, quas quibusdam. Atque, officia. Quod, quas
+          quibusdam. Atque, officia. Quod, quas quibusdam. Atque, officia. Quod,
+          quas quibusdam. Atque, officia. Quod, quas quibusdam. Atque, officia.
+          Quod, quas quibusdam. Atque, officia. Quod, quas quibusdam. Atque,
+          officia. Quod, quas quibusdam. Atque, officia. Quod, quas quibusdam.
+          Atque, officia. Quod, quas quibusdam. Atque, officia. Quod, quas
+          quibusdam. Atque, officia. Quod, quas quibusdam. Atque, officia. Quod,
+          quas quibusdam. Atque, officia. Quod, quas quibusdam. Atque, officia.
+        </p>
+      <Footer />
+    </Container>
+  );
 }
-export default About
+export default About;
