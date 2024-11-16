5598b7b67ad147ca2520e02c34774a2dc79545e4
yurikdotdev <172495341+yurikdotdev@users.noreply.github.com>
2024-11-16 18:01:54 +0700

    refactor(ui): update footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,15 @@ import { getYear } from '@/lib/utils';
 
 function Footer() {
   return (
-    <footer className="h-52 w-full bg-custom-bgLight font-newsreader text-custom-textLight dark:bg-custom-bgDark dark:text-custom-textDark">
+    <footer className="h-52 w-full bg-custom-bgLight text-custom-textLight dark:bg-custom-bgDark dark:text-custom-textDark">
       <div className="flex h-full w-full flex-col items-center justify-center gap-2">
-        <p className="font-medium">Trisolaris Roaster Co.</p>
-        <p className="text-sm">&copy; {getYear()}</p>
+        <p className="font-roboto text-sm font-light">
+          Designed and built by @yurikdotdev
+        </p>
+        <p className="font-newsreader text-3xl font-bold italic">
+          Trisolaris Roastery Co.
+        </p>
+        <p className="text-sm text-custom-accent">&copy; {getYear()}</p>
       </div>
     </footer>
   );
