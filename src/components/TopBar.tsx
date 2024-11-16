7bbd351031559b2704bb762a8e1a45e074ec0182
yurikdotdev <172495341+yurikdotdev@users.noreply.github.com>
2024-11-16 19:26:45 +0700

    refactor(ui):  update topbar styling

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -16,36 +16,73 @@ interface IconButtonProps {
 
 const HeaderLogo = () => (
   <Link to={ROUTES.HOME} className="flex items-center gap-2">
-    <LucideTriangle className="h-3 w-3 fill-custom-accent text-custom-textLight dark:text-custom-textDark" />
-    <h1 className="text-md font-inter font-bold tracking-tight">
+    <LucideTriangle className="h-4 w-4 transform fill-custom-accent text-custom-textLight transition-transform duration-300 ease-in-out hover:rotate-180 dark:text-custom-textDark" />
+    <h1 className="ease-in-ou font-inter text-lg font-semibold tracking-tight transition-colors duration-300 hover:text-custom-accent">
       Trisolaris Roastery Co.
     </h1>
   </Link>
 );
 
-const IconButton: React.FC<IconButtonProps> = ({ icon, onClick }) => (
+const ActionButton: React.FC<IconButtonProps> = ({ icon, onClick }) => (
   <button
-    className="rounded-lg bg-custom-surface p-2 text-custom-textLight"
+    className="rounded-lg bg-custom-surface p-3 text-custom-textLight shadow-sm shadow-stone-200 duration-300 ease-in-out hover:bg-custom-accent hover:font-bold hover:text-custom-bgLight dark:shadow-stone-800"
     onClick={onClick}
   >
     {React.cloneElement(icon, { className: 'h-5 w-4' })}
   </button>
 );
 
-const MobileDropdownButton: React.FC<{ onClick: () => void }> = ({
+const ResponsiveMenuButton: React.FC<{ onClick: () => void }> = ({
   onClick,
 }) => (
   <button
-    className="text-custom-textLight dark:text-custom-textDark md:hidden"
+    className="text-custom-textLight lg:hidden dark:text-custom-textDark"
     onClick={onClick}
   >
     <LucideMenu className="h-5 w-6" />
   </button>
 );
 
-const MobileDropdownMenu = ({ isMenuOpen }: { isMenuOpen: boolean }) => (
+const NavLinks = () => {
+  const NAV_LINKS = [
+    { label: 'About', route: ROUTES.ABOUT },
+    { label: 'Same Origins', route: ROUTES.PRODUCTS },
+    { label: 'Subscriptions', route: ROUTES.SUBSCRIPTION },
+    { label: 'Learn', route: ROUTES.LEARN },
+    { label: 'Locations', route: ROUTES.LOCATIONS },
+  ];
+
+  return (
+    <div className="hidden gap-8 font-inter text-sm font-semibold lg:flex">
+      {NAV_LINKS.map((link, idx) => (
+        <Link
+          key={idx}
+          to={link.route}
+          className="cursor-pointer duration-300 ease-in-out hover:text-custom-accent"
+        >
+          {link.label}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+const Actions = () => (
+  <div className="hidden items-center justify-center gap-2 lg:flex">
+    <button className="rounded-lg bg-custom-surface px-4 py-3 text-sm font-medium text-custom-textLight shadow-sm shadow-stone-200 duration-300 ease-in-out hover:bg-custom-accent hover:font-bold hover:text-custom-bgLight dark:shadow-stone-800">
+      <Link to={ROUTES.LOGIN}>Sign in</Link>
+    </button>
+    <ActionButton icon={<LucideSearch />} />
+    <Link to={ROUTES.CHECKOUT}>
+      {' '}
+      <ActionButton icon={<LucideShoppingBag />} />{' '}
+    </Link>
+  </div>
+);
+
+const ResponsiveMenu = ({ isMenuOpen }: { isMenuOpen: boolean }) => (
   <div
-    className={`absolute right-4 top-10 rounded-lg border-custom-textLight bg-custom-bgLight text-sm font-medium text-custom-textLight shadow-sm dark:border-custom-textDark dark:bg-custom-bgDark dark:text-custom-textDark md:hidden ${
+    className={`absolute right-4 top-10 rounded-lg border-custom-textLight bg-custom-bgLight text-sm font-medium text-custom-textLight shadow-sm dark:border-custom-textDark dark:bg-custom-bgDark dark:text-custom-textDark ${
       isMenuOpen ? 'block' : 'hidden'
     }`}
   >
@@ -78,30 +115,13 @@ function TopBar() {
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
   return (
-    <div className="sticky top-0 z-50 flex items-center justify-between bg-custom-bgLight px-6 py-4 dark:bg-custom-bgDark md:px-8 lg:px-10">
+    <div className="sticky top-0 z-50 flex items-center justify-between bg-custom-bgLight px-6 py-6 shadow-sm md:px-8 lg:px-10 dark:bg-custom-bgDark dark:shadow-stone-800">
       <HeaderLogo />
+      <NavLinks />
+      <Actions />
 
-      <div className="hidden items-center justify-center gap-2 md:flex">
-        <div className="flex gap-8 font-newsreader text-sm">
-          <Link to={ROUTES.ABOUT}>About</Link>
-          <p>Same Origins</p>
-          <p>Subscriptions</p>
-          <p>Learn</p>
-          <p>Locations</p>
-          <p></p>
-        </div>
-        <button className="rounded-lg bg-custom-surface px-4 py-2 text-sm font-medium text-custom-textLight">
-          <Link to={ROUTES.LOGIN}>Sign in</Link>
-        </button>
-        <IconButton icon={<LucideSearch />} />
-        <Link to={ROUTES.CHECKOUT}>
-          {' '}
-          <IconButton icon={<LucideShoppingBag />} />{' '}
-        </Link>
-      </div>
-
-      <MobileDropdownButton onClick={toggleMenu} />
-      <MobileDropdownMenu isMenuOpen={isMenuOpen} />
+      <ResponsiveMenuButton onClick={toggleMenu} />
+      <ResponsiveMenu isMenuOpen={isMenuOpen} />
     </div>
   );
 }
